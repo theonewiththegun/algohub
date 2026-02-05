6bf8ba9d140849769b3cbd2fdf82cc5c6ae70d77
Igor Popkov <popkov97.97.97@gmail.com>
2026-02-05 19:11:02 +0300

    feat(python): finalized the merge sort

--- python/mergesort.py
+++ python/mergesort.py
@@ -1,47 +1,56 @@
-def merge(l: list[int], r: list[int]):
+from random import randint
+
+
+def merge(left: list[int], right: list[int]) -> list[int]:
     l_i, r_i = 0, 0
     out = []
-    while l_i < len(l) and r_i < len(r):
-        if l[l_i] <= r[r_i]:
-            out.append(l[l_i])
+    while l_i < len(left) and r_i < len(right):
+        if left[l_i] <= right[r_i]:
+            out.append(left[l_i])
             l_i += 1
         else:
-            out.append(r[r_i])
+            out.append(right[r_i])
             r_i += 1
 
-    if l_i < len(l):
-        out.extend(l[l_i:])
-    if r_i < len(r):
-        out.extend(r[r_i:])
+    if l_i < len(left):
+        out.extend(left[l_i:])
+    if r_i < len(right):
+        out.extend(right[r_i:])
 
     return out
 
 
-def mergesort(x: list[int]):
+def mergesort(x: list[int]) -> list[int]:
     if len(x) <= 1:
         return x
 
     mid = len(x) // 2
-    l = mergesort(x[:mid])
-    r = mergesort(x[mid:])
+    left = mergesort(x[:mid])
+    right = mergesort(x[mid:])
 
-    return merge(l, r)
+    return merge(left, right)
+
+
+def genereate_test_case(list_len: int) -> tuple[list[int], list[int]]:
+    x = [randint(0, 100) for _ in range(list_len)]
+    return x, sorted(x)
 
 
 # ToDo move to pytest
 if __name__ == "__main__":
     test_cases = [
-        (list("9384756021"), list("0123456789")),
-        ([1, 2, 4, 4, 6, 5, 3, 3, 3, 3], [1, 2, 3, 3, 3, 3, 4, 4, 5, 6]),
+        genereate_test_case(1),
+        genereate_test_case(100),
+        genereate_test_case(10000),
     ]
     for i, case in enumerate(test_cases):
         try:
             res = mergesort(case[0])
             assert res == case[1]
             print(
-                f"PASS! Test #{i} passed. Input: {case[0]}, expected: {case[1]}, got: {res}"
+                f"PASS! Test # passed. Input: {case[0]}, expected: {case[1]}, got: {res}"
             )
-        except BaseException as e:
+        except BaseException:
             print(
                 f"FAIL! Test #{i} did not pass. Input: {case[0]}, expected: {case[1]}, got: {res}"
             )
